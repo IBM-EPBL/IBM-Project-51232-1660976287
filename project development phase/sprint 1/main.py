945c6361208c03c034c0ca75d596f4b1ed9c6d9c
vindhya6059 <113698237+vindhya6059@users.noreply.github.com>
2022-11-06 18:39:44 +0530

    Add files via upload

---
+++ project development phase/sprint 1/main.py
@@ -0,0 +1,29 @@
+# Python code
+
+# IMPORT SECTION STARTS
+
+import brain
+
+# IMPORT SECTION ENDS
+# -----------------------------------------------
+# USER INPUT SECTION STARTS
+
+myLocation = "Chennai,IN"
+APIKEY = "92eedd4b0b4cd6c543c365f562a59ab3"
+
+localityInfo = {
+    "schools" : {
+        "schoolZone" : True,
+        "activeTime" : ["7:00","17:30"] # schools active from 7 AM till 5:30 PM
+        },
+    "hospitalsNearby" : False,
+    "usualSpeedLimit" : 40 # in km/hr
+}
+
+# USER INPUT SECTION ENDS
+# -----------------------------------------------
+# MICRO-CONTROLLER CODE STARTS
+
+print(brain.processConditions(myLocation,APIKEY,localityInfo))
+
+# MICRO-CONTROLLER CODE ENDS
