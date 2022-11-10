bcb3a13cc9693e9b8a5120f3c7d985315f33ac6e
vindhya6059 <113698237+vindhya6059@users.noreply.github.com>
2022-11-10 15:45:21 +0530

    Add files via upload

---
+++ project development phase/sprint 2/sprint 2-code3.py
@@ -0,0 +1,37 @@
+import wiotp.sdk.device # python -m pip install wiotp
+import time
+
+# IMPORT SECTION ENDS
+# -----------------------------------------------
+# API CONFIG SECTION STARTS
+
+myConfig = {
+    "identity" : {
+        "orgId" : "epmoec",
+        "typeId" : "testDevice",
+        "deviceId" : "device0"
+    },
+    "auth" : {
+        "token" : "?-KDXUPMvDo_TK2&b1"
+    }
+}
+
+# API CONFIG SECTION ENDS
+# -----------------------------------------------
+# FUNCTIONS SECTION STARTS
+
+def myCommandCallback(cmd):
+    print("recieved cmd : ",cmd)
+
+
+def logData2Cloud(location,temperature,visibility):
+    client = wiotp.sdk.device.DeviceClient(config=myConfig,logHandlers=None)
+    client.connect()
+    client.publishEvent(eventId="status",msgFormat="json",data={
+        "temperature" : temperature,
+        "visibility" : visibility,
+        "location" : location
+    },qos=0,onPublish=None)
+    client.commandCallback = myCommandCallback
+    client.disconnect()
+    time.sleep(1)
